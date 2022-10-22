1aaa7a1abb5c1ef4550d47a2a4f6ed3a1455564c
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-10-21 21:38:51 -0300

    feat: change dtypes

--- ney.py
+++ ney.py
@@ -3,4 +3,18 @@ import numpy as np
 
 ney = pd.read_csv('https://raw.githubusercontent.com/cristiandarioortegayubro/base/main/dataset/neymar.csv')
 
-print(ney.head())
+# change dtypes
+ney['Tournament'] = ney['Tournament'].astype('string')
+ney['Matchday'] = ney['Matchday'].astype('string')
+ney['Date'] = ney['Date'].astype('datetime64[ns]')
+ney['Venue'] = ney['Venue'].astype('string')
+ney['Club'] = ney['Club'].astype('string')
+ney['Opponent'] = ney['Opponent'].astype('string')
+ney['Result'] = ney['Result'].astype('string')
+ney['Position'] = ney['Position'].astype('string')
+ney['Minute'] = ney['Minute'].astype('string')
+ney['When Scored'] = ney['When Scored'].astype('string')
+ney['Goal Type'] = ney['Goal Type'].astype('string')
+ney['Assist'] = ney['Assist'].astype('string')
+
+print(ney.dtypes)
