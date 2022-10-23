6dae64b80940a15abd49c9ee8c3ef3cb573c2097
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-10-23 13:22:50 -0300

    feat: data cleansing

--- ney.py
+++ ney.py
@@ -3,6 +3,7 @@ import numpy as np
 
 ney = pd.read_csv('https://raw.githubusercontent.com/cristiandarioortegayubro/base/main/dataset/neymar.csv')
 
+# data cleansing
 # change dtypes
 ney['Tournament'] = ney['Tournament'].astype('string')
 ney['Matchday'] = ney['Matchday'].astype('string')
@@ -17,4 +18,11 @@ ney['When Scored'] = ney['When Scored'].astype('string')
 ney['Goal Type'] = ney['Goal Type'].astype('string')
 ney['Assist'] = ney['Assist'].astype('string')
 
-print(ney.dtypes)
+# delete na
+ney.dropna(axis=0, inplace= True, thresh=int(ney.shape[1]*0.8))
+
+# add column, getting the values using the year of the date column
+ney['year'] = ney['Date'].dt.year
+print(ney.head())
+
+# data analysis
